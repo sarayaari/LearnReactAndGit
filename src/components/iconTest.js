62e9b3759fc996dddab9ff02a02a5cc0a2290cf8
sara-yaa <57491957+sarayaari@users.noreply.github.com>
2023-05-15 13:59:38 +0300

    end lesson 10

---
+++ src/components/iconTest.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BsFillCameraReelsFill } from "react-icons/bs"
+import { DiStreamline } from "react-icons/di";
+
+function IconTest() {
+  return (
+    <div>
+      IconTest
+      <br/>
+      <br/>
+      <BsFillCameraReelsFill style={{fontSize:"5em"}}/>
+      <DiStreamline  className='icon_classTest'/>
+    </div>
+  )
+}
+
+export default IconTest
