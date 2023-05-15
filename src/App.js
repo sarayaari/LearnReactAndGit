557d58ce239dabf3b137d1e4c034fc6123406519
sara-yaa <57491957+sarayaari@users.noreply.github.com>
2023-05-15 10:33:50 +0300

    second

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with git
         </a>
       </header>
     </div>
